172c8de6c6c570350400614498a997e59ea9d6d4
Xolani Dube <Xolanidube575@gmail.com>
2020-03-28 20:32:42 +0200

    Update sortme.py

--- sortme.py
+++ sortme.py
@@ -1,6 +1,6 @@
 # MIT License
 # 
-# Copyright (c) 2020 Xolani Dube and Surprise Ngoveni
+# Copyright (c) 2020 Xolani Dube 
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
